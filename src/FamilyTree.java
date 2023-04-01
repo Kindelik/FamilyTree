1522ead65602b45f3c74344f01a57cf67339a79f
Kindelik <117860891+Kindelik@users.noreply.github.com>
2023-04-01 22:51:30 +0300

    HomeWorkOne

---
+++ src/FamilyTree.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/* Создание класса в котором будет хранится весь перечень людей
+    и различные методы для работы с этим деревом
+    */
+public class FamilyTree {
+    List<Human> familyTree;
+
+    public FamilyTree(List<Human> familyTree) {
+        this.familyTree = familyTree;
+    }
+
+    public FamilyTree() {
+        this.familyTree = new ArrayList<>();
+    }
+
+    // Добавить новый объект Human в лист дерева
+    public void add(Human human) {
+        if (!familyTree.contains(human)) {
+            this.familyTree.add(human);
+            if (human.getFather() != null) human.getFather().addChildren(human);
+            if (human.getMather() != null) human.getMather().addChildren(human);
+        }
+    }
+
+    // Поиск человека по Фамилии в дереве
+    public List<Human> searchHuman(String lastName) {
+        List<Human> searchTemp = new ArrayList<>();
+        for (Human item : familyTree) {
+            if (item.getLastname().equals(lastName)) searchTemp.add(item);
+        }
+        return searchTemp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FamilyTree that = (FamilyTree) o;
+        return Objects.equals(familyTree, that.familyTree);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(familyTree);
+    }
+
+    @Override
+    public String toString() {
+        for (Human item : familyTree) {
+            System.out.println(item);
+        }
+        return " ";
+    }
+}
+
---
+++ src/Human.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Human {
+    private static int count = 1;
+    private final int id;
+    protected final String firstName;
+    protected final String lastname;
+    private final String dateOfBirth;
+    private final String dateOfDeath;
+    private final Sex sex;
+    private Human mather;
+    private Human father;
+    private final List<Human> childrens;
+
+
+    /** 
+     * @param firstName   Имя
+     * @param lastname    Фамилия
+     * @param dateOfBirth дата рождения (Формат записи "ГГГГ-ММ-ДД")
+     * @param sex         пол(male or female)
+     * @param mather      ссылка на объект мать Human(sex female)
+     * @param father      ссылка на объект отец Human(sex male)
+     */
+    public Human(String firstName, String lastname, String dateOfBirth, Sex sex, Human mather, Human father) {
+        this.id = count++;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = null;
+        this.sex = sex;
+        this.mather = mather;
+        this.father = father;
+        this.childrens = new ArrayList<>();
+    }
+
+    public Human(String firstName, String lastname, String dateOfBirth, Sex sex) {
+        this.id = count++;
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfDeath = null;
+        this.mather = null;
+        this.father = null;
+        this.childrens = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Human getMather() {
+        return mather;
+    }
+
+    public Human getFather() {
+        return father;
+    }
+
+    public List<Human> getChildrens() {
+        return childrens;
+    }
+
+    private void setMather(Human mather) {
+        this.mather = mather;
+    }
+
+    private void setFather(Human father) {
+        this.father = father;
+    }
+
+    /**Добавить ребенка в лист детей у человека*/
+    public void addChildren(Human human) {
+        if(!childrens.contains(human)) childrens.add(human);
+    }
+
+    @Override
+    public String toString() {
+        if (dateOfDeath != null) {
+            return " %s %s(id %d) \n  Дата рождения - %s' Дата сметри - %s'\n".formatted(firstName, lastname, id, dateOfBirth, dateOfDeath);
+        }
+        return " %s %s (id %d) \n  Дата рождения - %s \n".formatted(firstName, lastname, id, dateOfBirth);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return id == human.id && Objects.equals(firstName, human.firstName) && Objects.equals(lastname, human.lastname) && Objects.equals(dateOfBirth, human.dateOfBirth) && Objects.equals(dateOfDeath, human.dateOfDeath) && sex == human.sex;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastname, dateOfBirth, dateOfDeath, sex);
+    }
+}
+
